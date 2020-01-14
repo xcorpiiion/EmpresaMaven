73535dd9e4d3de4bb56263c32c1e47451c30eae9
Vinicius Cruz <vinicius.das.silva.cruz.sanji@gmail.com>
2020-01-14 11:49:02 -0300

    Teste iniciais na classe LojaTeste concluido

--- src/test/java/LojaTest.java
+++ src/test/java/LojaTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,30 +41,31 @@ public class LojaTest {
 		cliente.add(new Cliente("Dante", "dante@gmail.com", 900.00));
 		cliente.add(new Cliente("Harry", "harry@gmail.com", 1300.00));
 	}
+	
+	@Before
+	public void addDadosProduto() {
+		produto.add(new Produtos("Tablet", 2500.00, 50));
+		produto.add(new Produtos("Computador", 3500.00, 70));
+		produto.add(new Produtos("Smartphone", 2500.00, 150));
+		produto.add(new Produtos("Fone de Ouvido", 50.00, 200));
+	}
 
 	@Test(timeout = 10)
 	public void contratarFuncionario() {
 		if (funcionario == null) {
 			assertEquals("O funcionario está nullo", funcionario);
 		}
-
-		funcionario.add(new Funcionario("manoel", "jose@gmail.com", 2500.00, Cargo.Vendedor));
-
+		
+		String nome = "lalau";
+		String email = "lalau@gmail.com";
+		
 		// Verifica se o funcionario já existe
-		int aux = 0;
-		for (Funcionario func : this.funcionario) {
-			aux++;
-			for (int i = aux; i < this.funcionario.size(); i++) {
-				if (func.hashCode() == this.funcionario.get(i).hashCode()) {
-					if (func.equals(this.funcionario.get(i))) {
-						this.funcionario.remove(i);
-						assertTrue("Esse funcionario já foi contratado", false);
-					}
-				}
-			}
-		}
-
+		boolean wasContratado = this.funcionario.stream().anyMatch(func -> func.getNome().equalsIgnoreCase(nome) && 
+				func.getEmail().equalsIgnoreCase(email));
+		assertTrue("O funcionario já foi contratado", !wasContratado);
+		
 		// Caso o tenha passado na verificação, o funcionario será contratado
+		funcionario.add(new Funcionario(nome, email, 2500.00, Cargo.Vendedor));
 		System.out.println("O funcionario foi contratado");
 	}
 
@@ -75,9 +77,13 @@ public class LojaTest {
 
 		String nome = "Lucas";
 		String email = "lucas@gmail.com";
-		this.motivoDemissao = MotivoDemissao.Justa_Causa;
-		boolean wasDemitido = false;
 		
+		// Verifica se o funcionario existe
+		boolean wasDemitido = this.funcionario.stream().anyMatch(func -> func.getNome().equalsIgnoreCase(nome) && 
+				func.getEmail().equalsIgnoreCase(email));;
+		assertTrue("Não existe o funcionario com os dados informados", wasDemitido);
+		
+		this.motivoDemissao = MotivoDemissao.Justa_Causa;
 		switch(this.motivoDemissao) {
 		case Justa_Causa:
 			wasDemitido = this.funcionario.removeIf((func1 -> func1.getEmail().equalsIgnoreCase(email) && func1.getNome().equalsIgnoreCase(nome)));
@@ -86,43 +92,47 @@ public class LojaTest {
 			wasDemitido = this.funcionario.removeIf((func1 -> func1.getEmail().equalsIgnoreCase(email) && func1.getNome().equalsIgnoreCase(nome)));
 			break;
 		}
-		
-		if(!wasDemitido) {
-			assertFalse("Não existe o funcionario com os dados informados", true);
-		}
-		
+
+		// Caso o codigo passe no teste, ele vai demitir o funcionario
 		System.out.println("O funcionario foi demitido");
 
 	}
 
-	@Ignore
+	@Test(timeout = 50)
 	public void cadastrarCliente() {
-		if (funcionario == null) {
-			assertEquals("O funcionario está nullo", funcionario);
+		if (this.cliente == null) {
+			assertEquals("O cliente está nullo", this.cliente);
 		}
-
-		funcionario.add(new Funcionario("manoel", "jose@gmail.com", 2500.00, Cargo.Vendedor));
-
-		// Verifica se o funcionario já existe
-		int aux = 0;
-		for (Funcionario func : this.funcionario) {
-			aux++;
-			for (int i = aux; i < this.funcionario.size(); i++) {
-				if (func.hashCode() == this.funcionario.get(i).hashCode()) {
-					if (func.equals(this.funcionario.get(i))) {
-						this.funcionario.remove(i);
-						assertTrue("Esse funcionario já foi contratado", false);
-					}
-				}
-			}
-		}
+		
+		String nome = "lalau";
+		String email = "lalau@gmail.com";
+		
+		// Verifica se o cliente já existe
+		boolean wasCadastrado = this.cliente.stream().anyMatch(c -> c.getNome().equalsIgnoreCase(nome) && 
+				c.getEmail().equalsIgnoreCase(email));
+		assertTrue("O cliente já está cadastrado", !wasCadastrado);
 
 		// Caso o tenha passado na verificação, o funcionario será contratado
-		System.out.println("O funcionario foi contratado");
+		cliente.add(new Cliente(nome, email, 2500.00));
+		System.out.println("O cliente foi cadastrado");
 	}
 
-	@Ignore
+	@Test
 	public void cadastrarProduto() {
+		if (this.produto == null) {
+			assertEquals("O produto está nullo", this.produto);
+		}
+		
+		String nome = "lalau";
+		String email = "lalau@gmail.com";
+		
+		
+		// Verifica se o funcionario é um repositor
+		boolean isRepositor = this.funcionario.get(2).getCargo() == Cargo.Repositor;
+		assertTrue("Apenas repositores podem cadastra os produtos", isRepositor);
 
+		// Caso o tenha passado na verificação, o funcionario será contratado
+//		this.produto.add(new Produtos(nome, preco, estoque));
+		System.out.println("O produto foi cadastrado");
 	}
 }
