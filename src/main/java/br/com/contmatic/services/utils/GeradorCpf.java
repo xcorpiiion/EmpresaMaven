4f8f0130dc1effb4d47259b6ee7a86a74abf1c65
Vinicius da Silva Cruz <vinicius.cruz@CONTMATIC.AD>
2020-10-26 18:38:40 -0300

    Etapa 01 finish

--- src/main/java/br/com/contmatic/services/utils/GeradorCpf.java
+++ src/main/java/br/com/contmatic/services/utils/GeradorCpf.java
@@ -8,63 +8,61 @@ import java.util.Random;
 
 public final class GeradorCpf {
 
-    private static StringBuilder cpfRandom;
+	private static List<Integer> armazenaNumerosParaCalculoDoCpf;
 
-    private static List<Integer> armazenaNumerosParaCalculoDoCpf;
+	private static List<Integer> armazenaMultiplicacao = new ArrayList<>();
 
-    private static List<Integer> armazenaMultiplicacao = new ArrayList<>();
+	private static int resultadoModulo;
 
-    private static int resultadoModulo;
-    
-    private GeradorCpf() {
-    	
-    }
-    
-    public static String gerardorRandomCpf() {
-        cpfRandom = new StringBuilder();
-        armazenaNumerosParaCalculoDoCpf = new ArrayList<>();
-        geraNumerosAleatorios();
-        cpfRandom.append(armazenaNumerosParaCalculoDoCpf.get(9)).append(armazenaNumerosParaCalculoDoCpf.get(10));
-        if(!isCpfValid(cpfRandom.toString())) {
-        	gerardorRandomCpf();
-        }
-        return cpfRandom.toString();
-    }
+	private GeradorCpf() {
 
-    private static void geraNumerosAleatorios() {
-        for(int i = 0 ; i < 9 ; i++) {
-            armazenaNumerosParaCalculoDoCpf.add(1 + new Random().nextInt(9));
-            cpfRandom.append(armazenaNumerosParaCalculoDoCpf.get(i));
-        }
-        multiplicaNumeros(10);
-    }
+	}
+	
+	public static String gerardorRandomCpf() {
+		StringBuilder cpfRandom = new StringBuilder();
+		armazenaNumerosParaCalculoDoCpf = new ArrayList<>();
+		geraNumerosAleatorios(cpfRandom);
+		cpfRandom.append(armazenaNumerosParaCalculoDoCpf.get(9)).append(armazenaNumerosParaCalculoDoCpf.get(10));
+		if (!isCpfValid(cpfRandom.toString())) {
+			gerardorRandomCpf();
+		}
+		return cpfRandom.toString();
+	}
 
-    private static void multiplicaNumeros(int diminuiValores) {
-        armazenaMultiplicacao.clear();
-        for(int i = 0 ; i < armazenaNumerosParaCalculoDoCpf.size() ; i++) {
-            armazenaMultiplicacao.add(diminuiValores * armazenaNumerosParaCalculoDoCpf.get(i));
-            diminuiValores--;
-        }
-        somarValoresAndCalculaModulo();
-    }
+	private static void geraNumerosAleatorios(StringBuilder cpfRandom) {
+		for (int i = 0; i < 9; i++) {
+			armazenaNumerosParaCalculoDoCpf.add(1 + new Random().nextInt(9));
+			cpfRandom.append(armazenaNumerosParaCalculoDoCpf.get(i));
+		}
+		multiplicaNumeros(10);
+	}
 
-    private static void somarValoresAndCalculaModulo() {
-        resultadoModulo = 0;
-        for(int i = 0 ; i < armazenaMultiplicacao.size() ; i++) {
-            resultadoModulo += armazenaMultiplicacao.get(i);
-        }
-        resultadoModulo %= 11;
-        subtrairModuloPor11();
-    }
-    
-    /**
-     *  Nunca trocar o número 11 
-     */
-    private static void subtrairModuloPor11() {
-        if (armazenaNumerosParaCalculoDoCpf.size() < 11) {
-            armazenaNumerosParaCalculoDoCpf.add(11 - resultadoModulo);
-            multiplicaNumeros(11);
-        }
-    }
+	private static void multiplicaNumeros(int diminuiValores) {
+		armazenaMultiplicacao.clear();
+		for (int i = 0; i < armazenaNumerosParaCalculoDoCpf.size(); i++) {
+			armazenaMultiplicacao.add(diminuiValores * armazenaNumerosParaCalculoDoCpf.get(i));
+			diminuiValores--;
+		}
+		somarValoresAndCalculaModulo();
+	}
+
+	private static void somarValoresAndCalculaModulo() {
+		resultadoModulo = 0;
+		for (int i = 0; i < armazenaMultiplicacao.size(); i++) {
+			resultadoModulo += armazenaMultiplicacao.get(i);
+		}
+		resultadoModulo %= 11;
+		subtrairModuloPor11();
+	}
+
+	/**
+	 * Nunca trocar o número 11
+	 */
+	private static void subtrairModuloPor11() {
+		if (armazenaNumerosParaCalculoDoCpf.size() < 11) {
+			armazenaNumerosParaCalculoDoCpf.add(11 - resultadoModulo);
+			multiplicaNumeros(11);
+		}
+	}
 
 }
