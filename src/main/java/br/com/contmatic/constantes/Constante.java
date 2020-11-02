7196c6ba6f68025241cacca8539b98dcb2ac5694
Vinicius da Silva Cruz <vinicius.cruz@CONTMATIC.AD>
2020-11-02 18:56:03 -0300

    Update to etapa 2

--- src/main/java/br/com/contmatic/constantes/Constante.java
+++ src/main/java/br/com/contmatic/constantes/Constante.java
@@ -1,63 +1,185 @@
 package br.com.contmatic.constantes;
 
-import static java.lang.String.valueOf;
-
+/**
+ * The Class Constante.
+ */
 public final class Constante {
 
-	public static final String EMAIL_VALIDO = "([a-zA-Z0-9]+(?:[._+-][a-zA-Z0-9]+)*)@([a-zA-Z0-9]+(?:[.-][a-zA-Z0-9]+)*[.][a-zA-Z]{2,})";
+    /**
+     * The Constant ILLEGAL_WORD.
+     */
+    public static final String ILLEGAL_WORD = "^[a-zA-ZÁÂÃÀÇÉÊÍÓÔÕÚÜáâãàçéêíóôõúü]*$";
 
-	public static final String CLIENTE = "cliente";
+    /**
+     * The Constant ILLEGAL_NUMBER.
+     */
+    public static final String ILLEGAL_NUMBER = "^[0-9]*$";
 
-	public static final String EMPRESA = "empresa";
+    /**
+     * The Constant VALIDATION_EMAIL.
+     */
+    public static final String VALIDATION_EMAIL = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 
-	public static final String ENDERECO = "endereco";
+    /**
+     * The Constant PHONE_VALIDATION.
+     */
+    public static final String PHONE_VALIDATION = "(([0-9]{8})|([0-9]{9}))";
 
-	public static final String FUNCIONARIO = "funcionario";
+    public static final String VALID = "valid";
 
-	public static final String PRODUTO = "produto";
+    public static final String NOME_NULL = "nomeNull";
 
-	private static final String NOME_CLASSE = "nomeClasse";
+    public static final String NOME_EMPTY = "nomeEmpty";
 
-	private static final String NOME_CAMPO = "nomeCampo";
+    public static final String NOME_BLANK_SPACE = "nomeBlankSpace";
 
-	private Constante() {
+    public static final String NOME_LESS_3_CARACTER = "nomeLess3Caracter";
 
-	}
+    public static final String NOME_GREATER_CARACTER = "nomeGreaterCaracter";
 
-	public static String campoVazioOrNullMensagemPadrao(String nomeCampo, String nomeClasse) {
-		return "Campo nomeCampo de nomeClasse não pode ficar vazio".replace(NOME_CAMPO, nomeCampo).replace(NOME_CLASSE,
-				nomeClasse);
-	}
+    public static final String NOME_WITH_SPECIAL_CARACTER = "nomeWithSpecialCaracter";
 
-	public static String tamanhoCamposMensagemPadrao(int tamanhoMinimo, int tamanhoMaximo, String nomeCampo,
-			String nomeClasse) {
-		return "O campo nomeCampo de nomeClasse precisa ter um tamanho mínimo de tamanhoMinimo e um tamanho máximo de tamanhoMaximo"
-				.replace("tamanhoMinimo", nomeCampo).replace("tamanhoMaximo", nomeClasse)
-				.replace("min", valueOf(tamanhoMinimo)).replace("max", valueOf(tamanhoMaximo));
-	}
+    public static final String EMAIL_NULL = "emailNull";
 
-	public static String stringJustContainsWordMensagemPadrao(String nomeCampo, String nomeClasse) {
-		return "Campo nomeCampo de nomeClasse deve apenas possuir letras".replace(NOME_CAMPO, nomeCampo)
-				.replace(NOME_CLASSE, nomeClasse);
-	}
+    public static final String EMAIL_EMPTY = "emailEmpty";
 
-	public static String stringJustContainsWordAndNumberMensagemPadrao(String nomeCampo, String nomeClasse) {
-		return "Campo nomeCampo de nomeClasse não deve possuir caracteres especiais".replace(NOME_CAMPO, nomeCampo)
-				.replace(NOME_CLASSE, nomeClasse);
-	}
+    public static final String EMAIL_BLANK_SPACE = "emailBlankSpace";
 
-	public static String campoInvalidoMensagemPadrao(String nomeCampo, String nomeClasse) {
-		return "Campo nomeCampo de nomeClasse está invalido. Favor revisar o campo".replace(NOME_CAMPO, nomeCampo)
-				.replace(NOME_CLASSE, nomeClasse);
-	}
+    public static final String EMAIL_INVALID = "emailInvalid";
 
-	public static String dataInvalidaMensagem(String mensagem) {
-		return mensagem;
-	}
+    public static final String EMAIL_LESS_10_CARACTERES = "emailLess10Caracteres";
 
-	public static String isDataGreaterThanCurrent(String nomeCampo, String nomeClasse) {
-		return "Campo nomeCampo de nomeClasse não pode ser maior do que a data atual".replace(NOME_CAMPO, nomeCampo)
-				.replace(NOME_CLASSE, nomeClasse);
-	}
+    public static final String EMAIL_GREATER_100_CARACTERES = "emailGreater100Caracteres";
+
+    public static final String EMAIL_WITH_BLANK_SPACE_IN_WORD = "emailWithBlankSpaceInWord";
+
+    public static final String EMAIL_WITH_NUMBER_AFTER_ARROBA = "emailWithNumberAfterArroba";
+
+    public static final String EMAIL_WITHOUT_ARROBA = "emailWithoutArroba";
+
+    public static final String EMAIL_WITHOUT_PONTO_COM = "emailWithoutPontoCom";
+
+    public static final String EMAIL_WITHOUT_COM = "emailWithoutCom";
+
+    public static final String EMAIL_WITH_SPECIAL_CARACTER = "emailWithSpecialCaracter";
+
+    public static final String CNPJ_NULL = "cnpjNull";
+
+    public static final String CNPJ_BLANK_SPACE = "cnpjBlankSpace";
+
+    public static final String CNPJ_CONTAINS_WORD = "cnpjContainsWord";
+
+    public static final String CNPJ_WRONG_SIZE = "cnpjWrongSize";
+
+    public static final String CPF_NULL = "cpfNull";
+
+    public static final String CPF_BLANK_SPACE = "cpfBlankSpace";
+
+    public static final String CPF_EMPTY = "cpfEmpty";
+
+    public static final String CPF_INVALID = "invalidCpf";
+
+    public static final String CPF_CONTAINS_WORD = "cpfContainsWord";
+
+    public static final String CPF_WRONG_SIZE = "cpfWrongSize";
+
+    public static final String ENDERECO_NULL = "enderecoNull";
+
+    public static final String PRODUTO_NULL = "produtoNull";
+
+    public static final String FUNCIONARIO_NULL = "funcionarioNull";
+
+    public static final String CLIENTE_NULL = "clienteNull";
+
+    public static final String DATA_NASCIMENTO_NULL = "dataNascimentoNull";
+
+    public static final String TELEFONE_NULL = "telefoneNull";
+
+    public static final String CARGO_NULL = "cargoNull";
+
+    public static final String SALARIO_NULL = "salarioNull";
+
+    public static final String SALARIO_LESS_1 = "salarioLess1";
+
+    public static final String TIPO_CONTRATO_NULL = "tipoContratoNull";
+
+    public static final String CIDADE_NULL = "cidadeNull";
+
+    public static final String CIDADE_EMPTY = "cidadeEmpty";
+
+    public static final String CIDADE_BLANK_SPACE = "cidadeBlankSpace";
+
+    public static final String CIDADE_CONTAINS_NUMBER = "cidadeContainsNumber";
+
+    public static final String CIDADE_LESS_3_CARACTER = "cidadeLess3Caracter";
+
+    public static final String CIDADE_GREATER_CARACTER = "cidadeGreaterCaracter";
+
+    public static final String CIDADE_WITH_SPECIAL_CARACTER = "cidadeWithSpecialCaracter";
+
+    public static final String CIDADE_GREATER_50_CARACTER = "cidadeGreater50Caracter";
+
+    public static final String ESTADO_NULL = "estadoNull";
+
+    public static final String NUMERO_RESIDENCIA_LESS_THAN_ZERO = "numeroResidenciaLessThanZero";
+
+    public static final String RUA_NULL = "ruaNull";
+
+    public static final String RUA_EMPTY = "ruaEmpty";
+
+    public static final String RUA_BLANK_SPACE = "ruaBlankSpace";
+
+    public static final String RUA_LESS_3_CARACTER = "ruaLess3Caracter";
+
+    public static final String RUA_WITH_SPECIAL_CARACTER = "ruaWithSpecialCaracter";
+
+    public static final String RUA_GREATER_50_CARACTER = "ruaGreater50Caracter";
+
+    public static final String CEP_NULL = "cepNull";
+
+    public static final String CEP_BLANK_SPACE = "cepBlankSpace";
+
+    public static final String CEP_EMPTY = "cepEmpty";
+
+    public static final String CEP_CONTAINS_WORD = "cepContainsWord";
+
+    public static final String CEP_LENGTH_DIFFERENCE_8 = "cepLengthDifference8";
+
+    public static final String BAIRRO_NULL = "bairroNull";
+
+    public static final String BAIRRO_EMPTY = "bairroEmpty";
+
+    public static final String BAIRRO_BLANK_SPACE = "bairroBlankSpace";
+
+    public static final String BAIRRO_LESS_3_CARACTER = "bairroLess3Caracter";
+
+    public static final String BAIRRO_WITH_SPECIAL_CARACTER = "bairroWithSpecialCaracter";
+
+    public static final String BAIRRO_GREATER_50_CARACTER = "bairroGreater50Caracter";
+
+    public static final String PHONE_NULL = "phoneNull";
+
+    public static final String PHONE_EMPTY = "phoneEmpty";
+
+    public static final String PHONE_BLANK_SPACE = "phoneBlankSpace";
+
+    public static final String PHONE_INVALID_SIZE = "phoneInvalidSize";
+
+    public static final String TIPO_TELEFONE_NULL = "tipoTelefoneNull";
+
+    public static final String DDD_NULL = "dddNull";
+
+    public static final String PRECO_NULL = "precoNull";
+
+    public static final String PRECO_LESS_1 = "precoLess1";
+
+    public static final String ESTOQUE_LESS_0 = "estoqueLess0";
+
+
+    /**
+     * Instantiates a new constante.
+     */
+    private Constante() {
+    }
 
 }
